0e9c713573c325b9b5009ef93f725649c3efcfca
박성진 <seongjin@naturemobility.com>
2019-08-29 00:34:12 +0900

    [박성진] 자바 v

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,6 +1,31 @@
 public class SelectionSort {
 
-    public static void main(String[] args) {
+    public static void selectionSort(int[] arr) {
+        selectionSort(arr, 0);
+    }
 
+    public static void selectionSort(int[] arr, int start) {
+        if(start < arr.length - 1) {
+            int minIndex = start;
+            for(int i = minIndex; i < arr.length; i++) {
+                if(arr[i] < arr[minIndex]) minIndex = i;
+            }
+            swapArr(arr, start, minIndex);
+            selectionSort(arr, start + 1);
+        }
+    }
+
+    public static void swapArr(int[] arr, int start, int minindex) {
+        int tmp = arr[start];
+        arr[start] = arr[minindex];
+        arr[minindex] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 4, 2 ,1};
+        selectionSort(arr);
+        for(int result : arr) {
+            System.out.print(result + ", ");
+        }
     }
 }
