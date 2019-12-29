927467f3f2440a4d28f0dda9a2183fb6056b154c
preciouStar <seongjin605@users.noreply.github.com>
2019-12-29 22:42:46 +0900

    linkedlist node update

---
+++ src/data/structure/LinkedListMain.java
@@ -0,0 +1,58 @@
+package data.structure;
+
+
+class LinkedList {
+    Node header;
+
+    static class Node {
+        int data;
+        Node next = null;
+    }
+
+    LinkedList() {
+        header = new Node();
+    }
+
+    void append(int d) {
+        Node end = new Node();
+        end.data = d;
+        Node n = header;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    void delete(int d) {
+        Node n = header;
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+            } else {
+                n = n.next;
+            }
+        }
+    }
+
+    void retireve() {
+        Node n = header.next;
+        while (n.next != null) {
+            System.out.print(n.data + " -> ");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+}
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+        linkedList.retireve();
+        linkedList.delete(1);
+        linkedList.retireve();
+    }
+}
