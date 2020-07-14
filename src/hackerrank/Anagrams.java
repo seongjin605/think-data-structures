30cf1a22675debffe5c23b542635574563466a99
preciouStar <seongjin605@users.noreply.github.com>
2020-07-15 01:07:33 +0900

    ㅅ

---
+++ src/hackerrank/Anagrams.java
@@ -0,0 +1,18 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
