bc05c074b5bf724b1a40d6872f93554c59a5614c
preciouStar <seongjin605@users.noreply.github.com>
2020-07-22 22:57:07 +0900

    상향식 동적계획법

--- src/data/structure/Fibonacci.java
+++ src/data/structure/Fibonacci.java
@@ -7,20 +7,38 @@ public class Fibonacci {
 //        return fibonacci(i - 1) + fibonacci(i - 2);
 //    }
 
-    static int fibonacci(int n) {
-        return fibonacci(n, new int[n + 1]);
-    }
+    /**
+     * 하향식 동적계획법(메모제이션)
+     * @param n
+     * @return
+     */
+//    static int fibonacci(int n) {
+//        return fibonacci(n, new int[n + 1]);
+//    }
+//
+//    static int fibonacci(int i, int[] memo) {
+//        if (i == 0 || i == 1) return i;
+//        if (memo[i] == 0) {
+//            memo[i] = fibonacci(i - 1, memo)
+//                    + fibonacci(i - 2, memo);
+//        }
+//        return memo[i];
+//    }
+    // --------------------------------------------------------------------------------
 
-    static int fibonacci(int i, int[] memo) {
-        if (i == 0 || i == 1) return i;
-        if (memo[i] == 0) {
-            memo[i] = fibonacci(i - 1, memo)
-                    + fibonacci(i - 2, memo);
+    static int fibonacci(int n) {
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        int[] memo = new int[n];
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i = 2 ; i < n; i++) {
+            memo[i] = memo[i - 1] + memo[i -2];
         }
-        return memo[i];
+        return memo[n - 1] + memo[n -2];
     }
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(20));
     }
 }
