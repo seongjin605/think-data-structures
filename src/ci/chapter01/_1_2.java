4e9b1006cb1343f5cbae7e9b757cb06c5832cd58
preciouStar <seongjin605@users.noreply.github.com>
2020-07-27 00:43:34 +0900

    test

--- src/ci/chapter01/_1_2.java
+++ src/ci/chapter01/_1_2.java
@@ -1,7 +1,5 @@
 package ci.chapter01;
 
-import java.util.Arrays;
-
 public class _1_2 {
 
 //    public static boolean isPermutation(String str) {
@@ -23,23 +21,45 @@ public class _1_2 {
      * 만일 두 문자열이 서로 순열 관계라면, 이 둘은 같은 문자로 거성된 순서만 다를 것이다.
      * 어떤 면에서는 최적의 솔루션은 아니다. 하지만 깔끔하고 단순한하며 이해하기 쉽다는 측면이고
      * 효율성을 따지면 아래의 풀이법으로 접근하자.
+     *
      * @param s
      * @return
      */
-    public static String sort(String s) {
-        char[] content = s.toCharArray();
-        Arrays.sort(content);
-        return new String(content);
-    }
-
-    public static boolean permutation(String s, String t) {
-        if(s.length() != t.length()) {
-            return false;
-        }
-        return sort(s).equals(sort(t));
-    }
+//    public static String sort(String s) {
+//        char[] content = s.toCharArray();
+//        Arrays.sort(content);
+//        return new String(content);
+//    }
+//
+//    public static boolean permutation(String s, String t) {
+//        if(s.length() != t.length()) {
+//            return false;
+//        }
+//        return sort(s).equals(sort(t));
+//    }
 
     // ------------------------------------------------------------------------
+    public static boolean permutation(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] letters = new int[128];
+
+        char[] s_array = s.toCharArray();
+        for (char c : s_array) {
+            letters[c]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            int c = (int) t.charAt(i);
+            letters[c]--;
+            if (letters[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         System.out.println(permutation("ba", "ab"));
