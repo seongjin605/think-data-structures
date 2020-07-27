16143e85dd6d923368baa704baac8804f8b12b20
preciouStar <seongjin605@users.noreply.github.com>
2020-07-28 00:30:00 +0900

    compression

--- src/ci/chapter01/_1_6.java
+++ src/ci/chapter01/_1_6.java
@@ -5,27 +5,72 @@ public class _1_6 {
     /**
      * INPUT: aabcccaaa
      * OUTPUT: a2b1c3a3
-     *
-     * @param s
-     * @return
      */
-    static String compression(String s) {
-        if (s == null || "".equals(s)) throw new IllegalArgumentException();
-
-        char[] contents = s.toCharArray();
-//        for(int i = 0; i < contents.length; i++) {
-//            contents[i] =
+//    static String compression(String s) {
+//        if (s == null || "".equals(s)) throw new IllegalArgumentException();
+//
+//        String compressedString = "";
+//        int count = 0;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            count++;
+//            if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+//                compressedString += "" + s.charAt(i) + count;
+//                count = 0;
+//            }
 //        }
+//        return compressedString.length() < s.length() ? compressedString : s;
+//    }
 
+//    static String compression(String s) {
+//        if (s == null || "".equals(s)) throw new IllegalArgumentException();
+//
+//        StringBuilder compressed = new StringBuilder();
+//        int count = 0;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            count++;
+//            if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+//                compressed.append(s.charAt(i));
+//                compressed.append(count);
+//                count = 0;
+//            }
+//        }
+//        return compressed.length() < s.length() ? compressed.toString() : s;
+//    }
+    static String compression(String s) {
+        int finalLength = countCompression(s);
+        if (finalLength >= s.length()) return s;
+
+        StringBuilder compressed = new StringBuilder(finalLength);
         int count = 0;
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[0] == contents[i + 1]) count++;
-            else if(contents[i] == contents[i + 1]);
+
+        for (int i = 0; i < s.length(); i++) {
+            count++;
+            if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+                compressed.append(s.charAt(i));
+                compressed.append(count);
+                count = 0;
+            }
         }
-        return null;
+        return compressed.toString();
+    }
+
+    static int countCompression(String s) {
+        int compressionLength = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            count++;
+            if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+                compressionLength += 1 + count;
+                count = 0;
+            }
+        }
+        return compressionLength;
     }
 
     public static void main(String[] args) {
-        compression("aabcccaaa");
+        System.out.println(compression("aabcccaaa"));
     }
 }
