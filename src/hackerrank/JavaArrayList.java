ffb901db4a5e265c5fbef6a65b5550db77fb5b73
preciouStar <seongjin605@users.noreply.github.com>
2020-08-04 00:29:40 +0900

    better solution

--- src/hackerrank/JavaArrayList.java
+++ src/hackerrank/JavaArrayList.java
@@ -26,22 +26,28 @@ public class JavaArrayList {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int d, q, x, y;
-        ArrayList[] set = new ArrayList[n];
+        ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            d = in.nextInt();
-            set[i] = new ArrayList();
+            int d = in.nextInt();
+            ArrayList<Integer> row = new ArrayList<>();
+
             for (int j = 0; j < d; j++) {
-                set[i].add(in.nextInt());
+                row.add(in.nextInt());
             }
+
+            rows.add(row);
         }
-        q = in.nextInt();
+
+        int q = in.nextInt();
+
         for (int i = 0; i < q; i++) {
-            x = in.nextInt();
-            y = in.nextInt();
+            int x = in.nextInt();
+            int y = in.nextInt();
+
             try {
-                System.out.println(set[x - 1].get(y - 1));
-            } catch (Exception e) {
+                System.out.println(rows.get(x - 1).get(y - 1));
+            } catch (IndexOutOfBoundsException e) {
                 System.out.println("ERROR!");
             }
         }
