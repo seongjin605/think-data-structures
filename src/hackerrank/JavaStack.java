a68a833c144716a79c7dbc4164cabfb4dcb3ea3f
preciouStar <seongjin605@users.noreply.github.com>
2020-08-06 01:36:16 +0900

    balances stacky

--- src/hackerrank/JavaStack.java
+++ src/hackerrank/JavaStack.java
@@ -1,7 +1,5 @@
 package hackerrank;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -22,66 +20,27 @@ public class JavaStack {
     public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String input = sc.next();
-            if (input == null || "".equals(input)) throw new IllegalArgumentException();
-
-            char[] c = input.toCharArray();
-
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < c.length; i++) {
-                stack.push(c[i]);
-            }
-
-            Deque<Character> deque = new ArrayDeque<>(input.length());
-
-            int firstLeft = 0;
-            int firstRight = 0;
-
-            int secondLeft = 0;
-            int secondRight = 0;
-
-            int thirdLeft = 0;
-            int thirdRight = 0;
-
-            boolean result = true;
-            for (int i = 0; i < input.length(); i++) {
-                if (!stack.isEmpty()) {
-                    Character data = stack.pop();
-                    if (input.charAt(i) == '(' && data == ')') {
-                        deque.add(data);
-                    } else if (input.charAt(i) == '[' && data == ']') {
-                        deque.add(data);
-                    } else if (input.charAt(i) == '{' && data == '}') {
-                        deque.add(data);
-                    } else {
-                        deque.add(data);
-                        while (!deque.isEmpty() && deque.size() == input.length()) {
-                            if (deque.removeFirstOccurrence('(')) {
-                                firstLeft++;
-                                continue;
-                            } else if (deque.removeFirstOccurrence(')')) {
-                                firstRight++;
-                                continue;
-                            } else if (deque.removeFirstOccurrence('[')) {
-                                secondLeft++;
-                                continue;
-                            } else if (deque.removeFirstOccurrence(']')) {
-                                secondRight++;
-                                continue;
-                            } else if (deque.removeFirstOccurrence('{')) {
-                                thirdLeft++;
-                                continue;
-                            } else if (deque.removeFirstOccurrence('}')) {
-                                thirdRight++;
-                                continue;
-                            }
+                String input=sc.next();
+                Stack<Character> stacky = new Stack<>();
+                for (int i = 0; i < input.length(); i++) {
+                    if (!stacky.isEmpty()) {
+                        switch(input.charAt(i)) {
+                            case '}' : if (stacky.peek() == '{') {
+                                stacky.pop();
+                            } break;
+                            case ']' : if (stacky.peek() == '[') {
+                                stacky.pop();
+                            } break;
+                            case ')' : if (stacky.peek() == '(') {
+                                stacky.pop();
+                            } break;
+                            default: stacky.push(input.charAt(i));
                         }
+                    } else {
+                        stacky.push(input.charAt(i));
                     }
                 }
-            }
-
-            result = (firstLeft + firstRight) % 2 == 0 && (secondLeft + secondRight) % 2 == 0 && (thirdLeft + thirdRight) % 2 == 0;
-            System.out.println(result);
+                System.out.println(stacky.isEmpty());
         }
     }
 }
